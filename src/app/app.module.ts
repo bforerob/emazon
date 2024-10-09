190cd4b15f54d58c9cfa9193df53a3e3c2abc70f
bforerob <bforerob@unal.edu.co>
2024-10-09 14:53:52 -0500

    label-input atom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; // Asegúrate de importar ReactiveFormsModule
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/atoms/button/button.component';
+import { LabelInputComponent } from './components/atoms/label-input/label-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    LabelInputComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/atoms/label-input/label-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-label-input',
+  templateUrl: './label-input.component.html',
+  styleUrls: ['./label-input.component.scss']
+})
+export class LabelInputComponent {
+  @Input() label: string = '';
+  @Input() type: string = 'text';
+  @Input() formControl!: FormControl;
+  @Input() placeholder: string = '';
+}
